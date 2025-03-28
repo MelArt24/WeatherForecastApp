5d3aae2168d69761168ae68041939f0bc0f7692b
MelArt24 <animator2404@gmail.com>
2025-03-28 18:26:58 +0200

    Added files to Github

---
+++ app/src/main/java/com/am24/weatherforecastapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.am24.weatherforecastapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
