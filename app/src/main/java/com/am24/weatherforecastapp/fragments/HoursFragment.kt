1909dfc61b97173c64950e6fbe84072637fa7b83
MelArt24 <animator2404@gmail.com>
2025-04-09 19:22:13 +0300

    Updated HoursFragment

--- app/src/main/java/com/am24/weatherforecastapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/am24/weatherforecastapp/fragments/HoursFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.am24.weatherforecastapp.R
+import com.am24.weatherforecastapp.adapters.WeatherAdapter
+import com.am24.weatherforecastapp.adapters.WeatherModel
+import com.am24.weatherforecastapp.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRV()
+    }
+
+    private fun initRV() = with(binding) {
+        rvHours.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rvHours.adapter = adapter
+        adapter.submitList()
     }
 
     companion object {
