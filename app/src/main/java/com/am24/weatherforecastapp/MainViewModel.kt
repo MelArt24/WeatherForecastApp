401d99b9f2b6e102acf57df005a625827380222c
MelArt24 <animator2404@gmail.com>
2025-04-09 12:50:43 +0300

    Added WeatherModel and list_item.xml

--- app/src/main/java/com/am24/weatherforecastapp/MainViewModel.kt
+++ app/src/main/java/com/am24/weatherforecastapp/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.am24.weatherforecastapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.am24.weatherforecastapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val dataCurrent = MutableLiveData<String>()
-    val dataList = MutableLiveData<List<String>>()
+    val dataCurrent = MutableLiveData<WeatherModel>()
+    val dataList = MutableLiveData<List<WeatherModel>>()
 }
