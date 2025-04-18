bfad39c26ff7b9bf3ce79c4b9a2aeef6883d111f
MelArt24 <animator2404@gmail.com>
2025-04-18 12:56:54 +0300

    Add function for future transliteration

--- app/src/main/java/com/am24/weatherforecastapp/fragments/MainFragment.kt
+++ app/src/main/java/com/am24/weatherforecastapp/fragments/MainFragment.kt
@@ -183,6 +183,35 @@ class MainFragment : Fragment() {
         }
     }
 
+    private fun transliterate(text: String): String {
+        val map = mapOf(
+            'а' to "a", 'б' to "b", 'в' to "v", 'г' to "h", 'ґ' to "g",
+            'д' to "d", 'е' to "e", 'є' to "ye", 'ж' to "zh", 'з' to "z",
+            'и' to "y", 'і' to "i", 'ї' to "i", 'й' to "y", 'к' to "k",
+            'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p",
+            'р' to "r", 'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f",
+            'х' to "kh", 'ц' to "ts", 'ч' to "ch", 'ш' to "sh", 'щ' to "shch",
+            'ю' to "yu", 'я' to "ya", 'ь' to "", '’' to "", '\'' to "",
+
+            'А' to "A", 'Б' to "B", 'В' to "V", 'Г' to "H", 'Ґ' to "G",
+            'Д' to "D", 'Е' to "E", 'Є' to "Ye", 'Ж' to "Zh", 'З' to "Z",
+            'И' to "Y", 'І' to "I", 'Ї' to "Yi", 'Й' to "Y", 'К' to "K",
+            'Л' to "L", 'М' to "M", 'Н' to "N", 'О' to "O", 'П' to "P",
+            'Р' to "R", 'С' to "S", 'Т' to "T", 'У' to "U", 'Ф' to "F",
+            'Х' to "Kh", 'Ц' to "Ts", 'Ч' to "Ch", 'Ш' to "Sh", 'Щ' to "Shch",
+            'Ю' to "Yu", 'Я' to "Ya", 'Ь' to ""
+        )
+
+
+        val transliterated = text.map { char -> map[char] ?: char.toString() }.joinToString("")
+
+        Log.d("Transliteration", "Original: $text -> Transliterated: $transliterated")
+
+
+        return transliterated
+    }
+
+
     private fun requestWeatherData(city: String) {
         val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
                 WEATHER_API_KEY +
